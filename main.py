7b52e3af699edc2060d935d9b8acb64e3fec764f
vasantharaja200295 <vasantharaja200295@gmail.com>
2023-10-20 22:10:31 +0530

    v3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Test Heroku app"
+    return "Test vercel app"
 
 if (__name__) == "__main__":
-    app.run(debug=False, host='0.0.0.0', port='80')
+    app.run(debug=False)
